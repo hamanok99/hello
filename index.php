8ff44428a76c5490026f701d959aca9ef4725686
hamanok99 <93187685+hamanok99@users.noreply.github.com>
2021-10-27 13:25:23 +0900

    ファイル読込

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
  <?php echo '<p>Hello World</p>'; ?>
  <?php echo '<p>deploy test</p>'; ?>
  <?php echo '<p>OK</p>'; ?>
- </body>
+ <?php echo var_dump(file('/sample.txt')); ?>
+
+</body>
 </html>
-<?php
-file_put_contents("sample.txt", "HELLO WORLD");
-?>
