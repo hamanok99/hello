bcf2c602ce72a2e4dad2bc0770ea77517a23e306
hamanok99 <kazuki.hamano@wisdom.co.jp>
2021-11-05 17:01:10 +0900

    修正

--- index.php
+++ index.php
@@ -7,16 +7,37 @@
 </body>
 </html>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <?php
 
 $url = parse_url(getenv('DATABASE_URL'));
 $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
 $pdo = new PDO($dsn, $url['user'], $url['pass']);
-var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
 
 //$sql = "INSERT INTO public.test(id, name, email, age, designation, created) VALUES (1, '山田', 'yamada@gmail.com', 19, 'エンジニア', current_timestamp);";
 //$pdo->exec ($sql);
-
+/*
 $sql = 'select * from public.test';
 foreach ($pdo->query($sql) as $row) {
     echo $row['id'];
@@ -26,5 +47,5 @@ foreach ($pdo->query($sql) as $row) {
     echo $row['designation'];
     echo $row['created'];
 }
-
+*/
 ?>
