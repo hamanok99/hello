000abd78070eb48450554af0af7532f55c5eb632
hamanok99 <kazuki.hamano@wisdom.co.jp>
2021-11-05 15:35:47 +0900

    composer.json追加

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ffbcdd25f6cd04d096b9f5ca988d414076430303',
+        'name' => 'hello/hello',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hello/hello' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ffbcdd25f6cd04d096b9f5ca988d414076430303',
+            'dev_requirement' => false,
+        ),
+    ),
+);
