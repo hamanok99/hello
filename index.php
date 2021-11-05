31b7cf69d2ca3df2c47f9f9310acb19e56f1292f
hamanok99 <kazuki.hamano@wisdom.co.jp>
2021-11-05 16:04:39 +0900

    DB接続テスト

--- index.php
+++ index.php
@@ -14,4 +14,17 @@ $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1))
 $pdo = new PDO($dsn, $url['user'], $url['pass']);
 var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
 
+//$sql = "INSERT INTO public.test(id, name, email, age, designation, created) VALUES (1, '山田', 'yamada@gmail.com', 19, 'エンジニア', current_timestamp);";
+//$pdo->exec ($sql);
+
+$sql = "select * from public.test";
+foreach ($dbh->query($sql) as $row) {
+    echo $row['id'];
+    echo $row['name'];
+    echo $row['email'];
+    echo $row['age'];
+    echo $row['designation'];
+    echo $row['created'];
+}
+
 ?>
