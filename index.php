22ab38410ddb6aa08d644b6379f2ab79d7c4285b
hamanok99 <kazuki.hamano@wisdom.co.jp>
2021-11-08 14:35:05 +0900

    修正

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
  <form action="index.php" method="post">
  <input type="text" name="name" class="form-text" value="">
  </form>
- <?php echo '<p>こんにちは世界</p>'; ?>
- <?php echo var_dump(isset($_POST["name"])); ?>
+ <?php
+ echo '<p>こんにちは世界</p>';
+ echo var_dump(isset($_POST["name"]));
+ ?>
 </body>
 </html>
